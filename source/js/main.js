4e4979ec242df957e99fa960b1e95ce0fbbefda0
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-30 20:31:55 +0600

    styling abonements desktop

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -19,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    initTabs();
   });
 });
 
